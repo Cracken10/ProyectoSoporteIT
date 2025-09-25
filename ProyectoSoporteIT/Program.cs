99cd970cc4643bbe23d79828d56cde9c07455444
Sistema SILT <s.i.l.t@silt.com>
2025-09-25 08:55:20 -0600

    Primer Commit

--- ProyectoSoporteIT/Program.cs
+++ ProyectoSoporteIT/Program.cs
@@ -2,7 +2,7 @@ namespace ProyectoSoporteIT
 {
     internal static class Program
     {
-        /// <summary>
+        /// <summary>  
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
