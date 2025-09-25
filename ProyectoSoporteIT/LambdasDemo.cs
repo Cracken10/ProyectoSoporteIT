cb8bd82890f0172ae906b085a44193fa78301f47
Sistema SILT <s.i.l.t@silt.com>
2025-09-25 11:36:45 -0600

    Lamdas Void

---
+++ ProyectoSoporteIT/LambdasDemo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ProyectoSoporteIT
+{
+    public static class LambdasDemo
+    {
+        public static void DemoLambdas()
+        {
+            Action voidLambda = () => Console.WriteLine("Void lambda ejecutada");
+            voidLambda();
+            Func<dynamic> returnLambda = () => new { Prop = "Valor" };
+            var resultado = returnLambda();
+            Console.WriteLine($"Lambda retorno: {resultado.Prop}");
+        }
+    }
+}
